164f4a6fbad8ee183621f4093362dc494137bf62
Maedeh-Saeidi <saeidimdhw@gmail.com>
2023-12-10 18:51:46 +0330

    12-advanced functions part 2(But it is not completed.).

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -18,10 +18,10 @@ if (!score) {
 
 let isAutoPlaying = false;
 let intervald ;
-
+ // const autoPlay = () =>{}
 function autoPlay(){
   if(!isAutoPlaying){
-    intervalId = setInterval(function(){
+    intervalId = setInterval(() => {
     const playerMove = pickComputerMove();
     playGame(playerMove);
     }, 1000);
@@ -35,6 +35,32 @@ function autoPlay(){
   }
   
 }
+
+ document.querySelector('.js-rock-button')
+.addEventListener('click', () => {
+ playGame('rock');
+ });
+
+ document.querySelector('.js-paper-button').
+ addEventListener('click', ()=>{
+  playGame('paper');
+ });
+
+ document.querySelector('.js-scissors-button').
+ addEventListener('click', ()=>{
+  playGame('scissors');
+ })
+
+document.body.addEventListener('keydown', (event)=>{
+  if(event.key === 'r'){
+    playGame('rock');
+  }else if(event.key === 'p'){
+    playGame('paper');
+  }else if(event.key === 's'){
+    playGame('scissors');
+  }
+});
+
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -6,6 +6,12 @@ const todoList = [{
   dueDate: '2012-06-09'
 }];
 
+document.querySelector('.js-add-todo-button').
+addEventListener('click', ()=>{
+  addTodo();
+})
+
+
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
@@ -24,7 +30,7 @@ function addTodo() {
 
 function renderTodoList(){
 let todoListHTML = '';
-todoList.forEach(function(todoObject, index){
+todoList.forEach((todoObject, index) => {
   const {name, dueDate} = todoObject;
   const html = `
   <div>${name}</div>
@@ -32,8 +38,16 @@ todoList.forEach(function(todoObject, index){
   <button onclick ="
   todoList.splice(${index}, 1);
   renderTodoList();
-  " class="delete-todo-button" >Delete</button> `;
+  " class="delete-todo-button js-delete-todo-list-button">Delete</button> `;
   todoListHTML += html;
 document.querySelector('.js-todo-list').innerHTML = todoListHTML; 
+
+document.querySelectorAll('.js-delete-todo-button').
+forEach((deleteButton, index)=>{
+  deleteButton.addEventListener('click',()=>{
+    todoList.splice(index, 1);
+  renderTodoList();
+  } )
+});
 });
 }
