cf617a661f3ba7294888dcef1cd459e50ba46d32
Maedeh-Saeidi <saeidimdhw@gmail.com>
2023-12-16 18:31:47 +0330

    Prime number Algorithem

---
+++ Prime number.js
@@ -0,0 +1,19 @@
+// My solution + Amir's guide
+function primeNumber (num) {
+if( num === 2) return true;
+for(let i = 2 ; i < Math.round(num/2) ; i++){
+if(num % i === 0) return false;
+}
+return true;
+}
+console.log(primeNumber(121));
+// Big-o = O(n)
+
+function isPrime1(n){
+if( n < 0) return false;
+for(let i = 2 ; i < n ; i++ ){
+  if( n % i === 0 ) return false;
+}
+return true;
+}
+// Big-O = O(n)
