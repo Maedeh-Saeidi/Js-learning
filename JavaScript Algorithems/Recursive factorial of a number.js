ae9eb4f36ed6f2f8f4490095a5060db50993737e
Maedeh-Saeidi <saeidimdhw@gmail.com>
2023-12-18 15:02:16 +0330

    Recursive factorial of a number

---
+++ JavaScript Algorithems/Recursive factorial of a number.js
@@ -0,0 +1,19 @@
+// My solution :
+function recursiveFactorial(n){
+  if( n < 2){
+    return n ;
+  }
+return n * recursiveFactorial(n-1);
+}
+console.log(recursiveFactorial(5));
+//Big-O = O(n) 
+
+
+function recursiveFactorial1(n){
+  if( n === 0){
+    return 1;
+  }
+  return n * recursiveFactorial1(n-1);
+}
+console.log(recursiveFactorial1(5));
+//Big-O = O(n)
