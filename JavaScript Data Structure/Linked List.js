e0dedc334ac8bdcafe1bfc508e40c49ee78047bf
Maedeh-Saeidi <saeidimdhw@gmail.com>
2023-12-30 17:39:44 +0330

    Linked List: Class,Prepand,Print,Append,Insert,Remove and Remove value

---
+++ JavaScript Data Structure/Linked List.js
@@ -0,0 +1,137 @@
+class Node {
+  constructor(value){
+    this.value = value
+    this.next = null
+  }
+}
+class LinkedList {
+  constructor(){
+    this.head = null
+    this.size = 0
+  }
+
+  isEmpty(){
+    return this.size === 0
+  }
+  getSize(){
+    return this.size
+  }
+
+  //O(1)
+  prepend(value){
+    const node = new Node(value)
+    if(this.isEmpty()){
+      this.head = node
+    }else{
+      node.next = this.head
+      this.head = node
+    }
+    this.size++
+  }
+  insert(value, index){
+    if(index < 0 || index > this.size) return
+    if(index === 0) this.prepend(value)
+    else{
+  const node = new Node(value)
+  let prev = this.head
+  for(let i = 0 ; i < index-1 ; i++){
+    prev = prev.next
+  }
+  node.next = prev.next
+  prev.next = node
+  this.size++
+  }
+  }
+  removeFrom(index){
+    if (index < 0 || index >= this.size) return null
+    let removeNode
+      if(index === 0) {
+        removeNode = this.head
+        this.head = this.head.next
+      } 
+      else{
+       let prev = this.head
+       for(let i = 0; i < index - 1; i++){
+        prev = prev.next
+       }  
+       removeNode = prev.next
+       prev.next =removeNode.next     
+      }
+      this.size --
+      return removeNode.value
+    
+  }
+  removeValue(value){
+    if(this.isEmpty()) return null
+    if(this.head.value === value){
+      this.head = this.head.next
+      this.size --
+      return value
+    }else{
+      let prev = this.head
+      while(prev.next && prev.next.value !== value){
+        prev = prev.next
+      }
+      if(prev.next){
+        const removeNode = prev.next
+        prev.next = removeNode.next
+        this.size --
+        return value
+      }
+      return null
+    }
+  }
+  print(){
+    if(this.isEmpty()) console.log('The list is empty!')
+    else{
+      let curr = this.head
+      let listValues = ''
+      while(curr){
+        listValues += ` ${curr.value}`
+        curr = curr.next
+      }
+      console.log(listValues) 
+  }
+  }
+  //O(n)
+  append(value){
+    const node = new Node(value)
+    if(this.isEmpty()){
+      this.head = node
+    }else {
+      let prev = this.head
+      while(prev.next){
+        prev = prev.next
+      }
+      prev.next = node
+    }
+    this.size ++
+  }
+}
+const list = new LinkedList()
+console.log('List is empty?' , list.isEmpty())
+console.log('List size', list.getSize())
+
+
+list.print();
+list.prepend(10);
+console.log(list.head)
+
+list.print();
+list.append(20);
+list.append(30);
+list.print();
+
+list.append(40);
+list.print();
+
+list.insert(10,0)
+list.print()
+console.log(list.removeFrom(10));
+console.log(list.removeFrom(0));
+list.print();
+console.log(list.removeFrom(1));
+list.print();
+
+console.log(list.removeValue(30))
+list.print();
